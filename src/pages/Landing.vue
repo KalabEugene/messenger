b851e09bdd8dcd6cf1649288a66e376cc649d6c9
KalabEugene <eugene.kalabuha@gmail.com>
2021-12-13 18:20:29 +0200

    master

---
+++ src/pages/Landing.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <div class="container__landing">
+            <img src="@/assets/images/back.jpg" alt="">
+            
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+.container__landing{
+    max-width: 100%;
+    max-height: 100%;
+    
+}
+</style>
