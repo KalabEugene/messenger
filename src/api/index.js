09c3f76409201accb578ee5872d1c207a64e52df
KalabEugene <eugene.kalabuha@gmail.com>
2022-02-01 23:07:44 +0200

    master

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import router from "../router/routes.js";
 
 const http = axios.create({
-  baseURL: process.env.VUE_APP_API_URL || "http://localhost:3000",
+  baseURL: process.env.VUE_APP_API_URL || "https://localhost:3000",
   withCredentials: false,
   crossDomain: true,
 });
