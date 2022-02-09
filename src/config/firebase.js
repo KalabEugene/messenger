0d2b707868c41d767d641deb69adcc2f5279bcc4
KalabEugene <88507648+KalabEugene@users.noreply.github.com>
2022-02-09 22:25:57 +0200

    Delete firebase.js

--- src/config/firebase.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: process.env.VUE_APP_API_KEY,
-  authDomain: "pekker.firebaseapp.com",
-  projectId: "pekker",
-  storageBucket: "pekker.appspot.com",
-  messagingSenderId: "148257193162",
-  appId: "1:148257193162:web:a6e16d68a0aba066748833",
-};
